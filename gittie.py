7289cad4ea73cdfb0579468dd426fb8ecc6cd984
PrzemekKondziela <kondziela.p@gmail.com>
2019-02-26 09:46:20 +0100

    def shout

--- gittie.py
+++ gittie.py
@@ -5,7 +5,12 @@ def introduce():
 def joke():
     print('What did 0 say to the 8 ? Nice belt !')
 
-    
+
 def add(a, b):
     c = a + b
     return c
+
+def shout():
+    print('AAAAAAAAA!!!!!')
+
+    
